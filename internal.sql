47c0e4524fc66b7c3d2a43ef969acb1f5edd7b13
Gouhigashi <tyler.nakazaki503@gmail.com>
2024-11-18 12:26:27 -0600

    Update internal.sql
    Fixes some of the issues with running the sql

--- internal.sql
+++ internal.sql
@@ -1,22 +1,27 @@
+DROP TABLE Shipping;
+DROP TABLE Orders;
+DROP TABLE Customer;
+DROP TABLE Inventory;
+
 CREATE TABLE Customer
 (
     customer_id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(50)
-    last_name VARCHAR(50)
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
     email VARCHAR(50) UNIQUE,
-    city VARCHAR(50)
-    user_state VARCHAR(2)
-    zip VARCHAR(50)
-    phone VARCHAR(50);
+    city VARCHAR(50),
+    user_state VARCHAR(2),
+    zip VARCHAR(50),
+    phone VARCHAR(50),
 
     PRIMARY KEY (customer_id)
 );
 
 CREATE TABLE Inventory
 (
-    item_id INT
-    item_desc VARCHAR(50)
-    quantity INT
+    item_id INT NOT NULL,
+    item_desc VARCHAR(50),
+    quantity INT,
 
     PRIMARY KEY (item_id)
 );
@@ -24,25 +29,25 @@ CREATE TABLE Inventory
 CREATE TABLE Orders
 (
     order_id INT NOT NULL AUTO_INCREMENT,
-    customer_id INT
-    order_status VARCHAR(20)
-    price FLOAT(8,2)
-    order_weight FLOAT(8,2)
-    rate FLOAT(8,2)
-
-    PRIMARY KEY (order_id)
+    customer_id INT NOT NULL,
+    order_status VARCHAR(20),
+    price FLOAT(8,2),
+    order_weight FLOAT(8,2),
+    rate FLOAT(8,2),
+    
+    PRIMARY KEY (order_id),
     FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
 );
 
 CREATE TABLE Shipping
 (
-    shipping_id INT NOT NULL AUTO_INCREMENT
-    order_id INT
-    item_id INT
-    item_name VARCHAR(50)
-    quantity INT
+    shipping_id INT NOT NULL AUTO_INCREMENT,
+    order_id INT NOT NULL,
+    item_id INT NOT NULL,
+    item_name VARCHAR(50),
+    quantity INT,
 
-    PRIMARY KEY (shipping_id)
-    FOREIGN KEY (order_id) REFERENCES Order(order_id)
+    PRIMARY KEY (shipping_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (item_id) REFERENCES Inventory(item_id)
 );
