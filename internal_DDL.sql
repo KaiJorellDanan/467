cde09964ebc419e95fa2a485304569ec8e2fa9b0
Gouhigashi <tyler.nakazaki503@gmail.com>
2024-12-02 12:41:28 -0600

    Update internal_DDL.sql

--- internal_DDL.sql
+++ internal_DDL.sql
@@ -1,4 +1,3 @@
-DROP TABLE Shipping;
 DROP TABLE Cart;
 DROP TABLE Shipping_cost;
 DROP TABLE Orders;
@@ -11,10 +10,7 @@ CREATE TABLE Customer
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     email VARCHAR(50) UNIQUE,
-    city VARCHAR(50),
-    user_state VARCHAR(2),
-    zip VARCHAR(50),
-    phone VARCHAR(50),
+    address VARCHAR(255),
 
     PRIMARY KEY (customer_id)
 );
@@ -32,25 +28,12 @@ CREATE TABLE Orders
     order_id INT NOT NULL AUTO_INCREMENT,
     customer_id INT NOT NULL,
     order_status VARCHAR(20),
+    order_date DATE,
     price FLOAT(8,2),
     order_weight FLOAT(8,2),
     
     PRIMARY KEY (order_id),
     FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
-
-);
-
-CREATE TABLE Shipping
-(
-    shipping_id INT NOT NULL AUTO_INCREMENT,
-    order_id INT NOT NULL,
-    item_id INT NOT NULL,
-    item_name VARCHAR(50),
-    quantity INT,
-
-    PRIMARY KEY (shipping_id),
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (item_id) REFERENCES Inventory(item_id)
 );
 
 CREATE TABLE Cart
